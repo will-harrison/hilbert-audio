eb5f99a3868bb6bd36cffef30f2f0286d7b6a917
Will Harrison <willharrison@gmail.com>
2020-02-03 15:31:02 -0800

    WIP, oneHilbert

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ const randRange = (min, max) => {
   return Math.random() * (max - min) + min
 }
 
-let H2 = h.Hilbert2d
-let h2 = new H2()
+  let H2 = h.Hilbert2d
+  let h2 = new H2()
 
 class App extends Component {
   state = {
+    listen: false,
     height: window.innerHeight,
     width: window.innerWidth,
     points: new Uint8Array(),
-    scale: 10,
+    scale: 50,
     drawHilbert: true,
     linePoints: [],
     stream: null,
@@ -45,8 +46,8 @@ class App extends Component {
     analyzer.smoothingTimeConstant = 0.3;
     analyzer.fftSize = 2048;
     const bufferLength = analyzer.frequencyBinCount
-    const hilbertSize = bufferLength * 8 / 256
-    console.log(bufferLength)
+    const oneHilbert = 16    
+    const hilbertSize = bufferLength / oneHilbert
     let dataArray = new Uint8Array(bufferLength)
     input.connect(analyzer)
 
@@ -59,12 +60,13 @@ class App extends Component {
 
     const draw = () => {
       let drawVisual = requestAnimationFrame(draw)
-      analyzer.getByteTimeDomainData(dataArray)
+  
+      this.state.listen && analyzer.getByteFrequencyData(dataArray)
 
       let value = 0
       let offset = 0
       let points = []
-      for (let i = 0; i < dataArray.length; i++) {
+      for (let i = 0; i < dataArray.length / 16; i++) {
         points.push({
           point: offset,
           vector: this.hilbertPointToXY(dataArray[i]),
@@ -81,10 +83,11 @@ class App extends Component {
       })
     }
 
-    // analyzer.connect(audioCtx.destination)
-    draw()
-    // this.draw(bufferLength)
+    analyzer.connect(audioCtx.destination)
+    
     this.drawHilbert(hilbertSize)
+    this.state.listen && draw()
+    // this.draw(bufferLength)
   }
 
 
@@ -127,7 +130,7 @@ class App extends Component {
     let { drawHilbert, width, scale } = this.state
     let linePoints = []
     if (drawHilbert) {
-      for (let i = 0; i < 256 * bufferLength; i++) {
+      for (let i = 0; i < 1 * bufferLength; i++) {
         let blah = this.hilbertPointToXY(i)
         linePoints.push(blah.x * scale)
         linePoints.push(blah.y * scale)
@@ -173,24 +176,141 @@ class App extends Component {
     let { height, linePoints, points, scale, width } = this.state
     let hilbertCurve = new Konva.Line({
       points: linePoints,
-      stroke: 'rgb(51, 51, 51)',
+      stroke: 'rgb(255, 255, 255)',
 
     })
+
+
+    
+    const tester = [
+      {
+        point: 0,
+        vector: {x: 0, y: 0},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 1,
+        vector: {x: 1, y: 0},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 2,
+        vector: {x: 0, y: 1},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 3,
+        vector: {x: 1, y: 1},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 4,
+        vector: {x: 0, y: 2},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 5,
+        vector: {x: 0, y: 3},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 6,
+        vector: {x: 1, y: 3},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 7,
+        vector: {x: 1, y: 2},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 8,
+        vector: {x: 2, y: 2},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 9,
+        vector: {x: 2, y: 3},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 10,
+        vector: {x: 3, y: 3},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 11,
+        vector: {x: 3, y: 2},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 12,
+        vector: {x: 3, y: 1},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 13,
+        vector: {x: 2, y: 1},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 14,
+        vector: {x: 2, y: 0},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+      {
+        point: 16,
+        vector: {x: 3, y: 0},
+        color: "rgba(0, 0, 0, .5)",
+        value: 87
+      },
+    ]
+    
     return (
+      <Background>
+
       <HilbertStage width={width} height={height} >
         <Layer>
           <Line {...hilbertCurve} />
-          {points && points.map((p, index) => {
+          {tester.map((p, index) => {
+            
             return <Circle x={p.vector.x * scale} y={p.vector.y * scale} radius={Math.max(p.value / 10, 0)} fill={p.color} key={index} />
           })}
         </Layer>
       </HilbertStage>
+          </Background>
     );
   }
 }
 
+const Background = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: #336699;
+`
+
 const HilbertStage = styled(Stage)`
   border: 1px solid black;
+  background-color: #336699;
+  position: absolute;
+  top: 0;
+  left: 0;
 `
+
 
 export default App;
---
+++ src/Hilbert.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import h from 'hilbert'
+let H2 = h.Hilbert2d
+let h2 = new H2()
+
+const Hilbert = () => {
+  const thisHilbert = 
+  return (
+  <div>
+
+  </div>
+  )
+}
